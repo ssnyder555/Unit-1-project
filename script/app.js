d26eddfeef514294406f54f6c6f138b09a1b0813
Stephen Snyder <ssnyder555@live.com>
2018-10-01 15:40:02 -0500

    shuffled complete

--- script/app.js
+++ script/app.js
@@ -1,8 +1,38 @@
-console.log('working');
-// cards array holds all cards
 let card = document.getElementsByClassName("card");
 let cards = [...card];
-// loop to add event listeners to each card
-for (var i = 0; i < cards.length; i++){
-   cards[i].addEventListener("click", displayCard);
+
+// loop to add event listiner to each cards
+for (var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", displayCard);
 };
+
+// toggles open and show class to display cards
+var displayCard = function (){
+  this.classList.toggle("open");
+  this.classList.toggle("show");
+  this.classList.toggle("disabled");
+}
+
+// Fisher-Yates (aka Knuth) Shuffle
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+// deck of all cards in game
+const deck = document.querySelector(".deck");
+function startGame(){
+   var shuffledCards = shuffle(cards);
+   for (var i= 0; i < shuffledCards.length; i++){
+      [].forEach.call(shuffledCards, function(item){
+         deck.appendChild(item);
+      });
+   }
+}
